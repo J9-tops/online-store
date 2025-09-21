46825312015dc62824c7d0b18185cd63907e93e4
J9-tops <j9.tops@gmail.com>
2025-09-21 22:47:24 +0100

    chore: remove unused variables

--- redux/store.ts
+++ redux/store.ts
@@ -2,12 +2,10 @@ import cartSlice from "@/redux/features/cart/cartSlice";
 import { authModalReducer } from "@/redux/features/modals";
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
-import categoryReducer from "./features/vendor/vendorSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    vendorCategory: categoryReducer,
     authModal: authModalReducer,
     cart: cartSlice,
   },
